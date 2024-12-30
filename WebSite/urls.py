ad748bdc702635dd393db1ccd47a161a2b2a6427
Ciscobot <victorcanodev@gmail.com>
2024-12-30 12:45:36 -0300

    Nuevo Diseño Alternativo

--- WebSite/urls.py
+++ WebSite/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name= 'Home'),
+    path('', views.index, name= 'Home'),
     path('QuienesSomos',views.quienes_somos, name = 'QuienesSomos'),
     path('Inmobiliaria',views.inmobiliaria, name = 'Inmobiliaria'),
     path('Regaleria',views.regaleria, name = 'Regaleria'),
-    path('Contacto',views.contacto, name = 'Contacto'),
+    #path('Contacto',views.contacto, name = 'Contacto'),
+    path('Galeria', views.galeria, name = 'Galeria'),
+    path('SobreNosotros', views.sobrenosotros, name = 'SobreNosotros'),
+    path('Productos', views.productos, name = 'Productos'),
+    path('Contacto', views.contac, name = 'Contacto'),
 ]
--- WebSite/views.py
+++ WebSite/views.py
@@ -17,3 +17,19 @@ def regaleria(request):
 
 def contacto(request):
     return render(request, 'plantillas/contacto.html')
+
+######################### PLANTILLAS #########################
+def index(request):
+    return render(request, 'plantillas/index.html')
+
+def galeria(request):
+    return render(request, 'plantillas/gallery.html')
+
+def sobrenosotros(request):
+    return render(request, 'plantillas/about.html')
+
+def productos(request):
+    return render(request, 'plantillas/products.html')
+
+def contac(request):
+    return render(request, 'plantillas/contact.html')
